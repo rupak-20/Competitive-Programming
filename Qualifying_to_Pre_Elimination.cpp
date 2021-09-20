4be53947a5f1de47bd0849b29a1fd1f4d7edd5d8
rupak-20 <rupakswami43887@gmail.com>
2021-09-21 00:08:00 +0530

    CodeChef QUALPREL

---
+++ Qualifying_to_Pre_Elimination.cpp
@@ -0,0 +1,92 @@
+//Qualifying_to_Pre_Elimination
+//https://www.codechef.com/problems/QUALPREL
+
+//#include<bits/stdc++.h>
+
+#include<vector>
+//#include<queue>
+//#include<stack>
+#include<set>
+//#include<map>
+//#include<unordered_set>
+//#include<unordered_map>
+//#include<deque>
+//#include<utility>
+
+//#include<priority_queue>
+
+#include<iostream>
+#include<algorithm>
+//#include<string>
+//#include<stdlib.h>
+//#include<math.h>
+//#include<time.h>
+#include<climits>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define all(x) x.begin(), x.end()
+
+// loop
+#define fo(a, b, c) for (int(a) = (b); (a)<(c); ++(a))
+#define foe(a, b, c) for (int(a) = (b); (a)<=(c); ++(a))
+#define foge(a, b, c) for (int(a) = (b); (a)>=(c); --(a))
+#define fosq(a, b, c) for (int(a) = (b); (a)*(a) <= (c); ++(a))
+
+#define foc(a, b, c) for (char(a) = (b); (a)<(c); ++(a))
+#define foce(a, b, c) for (char(a) = (b); (a)<=(c); ++(a))
+
+#define foat(a, b) for (auto &(a) : (b))
+#define foall(a, b) for (auto (a) : (b))
+#define range(i, n) fo(i, 0, n)
+
+// util
+#define sqr(x) ((x) * (x))
+#define minele(a) *min_element(all(a))
+#define maxele(a) *max_element(all(a))
+#define sum(a) accumulate(all(a), 0)
+
+
+
+//driver code
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int Test; cin >> Test;
+    while(Test--) {
+        //solution start
+
+        int n, k, temp; cin>>n>>k;
+        vi t(n); foat(i, t) cin>>i;
+        sort(all(t),greater<int>());
+        int teams = k;
+        fo(i, k, n) {
+            if(t[i]==t[k-1])
+            teams++;
+            else break;
+        }
+
+        cout<<teams<<"\n";
+
+        //solution end
+    }
+
+    return 0;
+}
